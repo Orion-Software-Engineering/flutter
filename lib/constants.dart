d693cfe4ad04097c4990759b946b2d7edf46abc2
kwasimbnyarko <51910665+kwasimbnyarko@users.noreply.github.com>
2022-07-03 13:15:21 +0000

    Worked on font and general replication of prototype. Created constants.dart as well.

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color signUpLoginTextColor = Colors.grey;
+const Color signUpLoginOrange = Color(0xFFcd5d27);
+const TextStyle signUpLoginTextFieldTextStyle = TextStyle(
+  color: signUpLoginTextColor,
+  fontFamily: 'Nunito',
+);
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,104 +1,148 @@
 // ignore_for_file: prefer_const_constructors
 //import 'passwordfield.dart'
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 class LoginPage extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
   RegExp emailValid = RegExp(
       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-  RegExp userNameValid = RegExp(
-      r"^[a-zA-Z0-9_]*$");
+  RegExp userNameValid = RegExp(r"^[a-zA-Z0-9_]*$");
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(15.0),
+        height: double.infinity,
+        padding: const EdgeInsets.fromLTRB(20, 100.0, 20.0, 8.0),
         width: double.infinity,
         child: Center(
           child: Form(
             key: _formKey,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  padding: EdgeInsets.fromLTRB(60.0, 0.0, 60.0, 0.0),
-                  child: Text('Sign Up',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 50.0,
-                    ),),
-                ),
-                Container(
-                  child: Text('Create your account to start matching',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 20.0,
-                  ),),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.grey,
-                        )
-                    ),
-                  ),
-                  validator: (value) {
-                    if (userNameValid.hasMatch(value!) && value.isNotEmpty) {
-                      return null;
-                    } else {
-                      return "Username should be alphanumeric";
-                    }
-                  },
-                ),
-
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.grey,
-                        )
-                    ),
-                  ),
-                  validator: (value) {
-                    if (emailValid.hasMatch(value!)) {
-                      return null;
-                    } else {
-                      return "Enter a valid email address";
-                    }
-                  },
-                ),
-                SizedBox(height: 60.0),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                      padding: EdgeInsets.fromLTRB(190.0, 10.0, 190.0, 10.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50.0),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        'Create your account to start matching',
+                        style: TextStyle(
+                          color: signUpLoginTextColor,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w700,
+                        ),
                       )
-                    ),
-                    onPressed: () {
-                      if (!_formKey!.currentState!.validate()) {
-                        return;
-                      }
-                    },
-                    child: Text('SIGN UP',
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.white,
-                    ),),
-                ),
-                SizedBox(height: 30.0),
-                Container(
-                  child: Text('Already have an account?',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
+                    ],
                   ),
-                )
+                ),
+                SizedBox(height: 80.0),
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          labelStyle: signUpLoginTextFieldTextStyle,
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: signUpLoginTextColor,
+                            ),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (userNameValid.hasMatch(value!) &&
+                              value.isNotEmpty) {
+                            return null;
+                          } else {
+                            return "Username should be alphanumeric";
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: signUpLoginTextFieldTextStyle,
+                          border: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                            color: Colors.grey,
+                          )),
+                        ),
+                        validator: (value) {
+                          if (emailValid.hasMatch(value!)) {
+                            return null;
+                          } else {
+                            return "Enter a valid email address";
+                          }
+                        },
+                      ),
+                      SizedBox(height: 60.0),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.black,
+                            // padding: EdgeInsets.fromLTRB(190.0, 10.0, 190.0, 10.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50.0),
+                            )),
+                        onPressed: () {
+                          if (!_formKey!.currentState!.validate()) {
+                            return;
+                          }
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 5.0, horizontal: 0.0),
+                          child: Center(
+                            child: Text(
+                              'SIGN UP',
+                              style: TextStyle(
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Already have an account?',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                          MaterialButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Color(0xFFcd5d27),
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16.0,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
