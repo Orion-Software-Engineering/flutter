77c7d561c6f08e726c1c2d55255ce5fd19dfe481
Dillys Annan <dillysannan@gmail.com>
2022-07-09 17:01:03 +0000

    signup api service and model included

---
+++ lib/api/api_service_signup.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:matchmaking_demo/constants.dart';
+import 'package:matchmaking_demo/models/signup_model.dart';
+import 'dart:convert';
+
+
+class APIService{
+
+  Future<SignupResponseModel> signup(SignupRequestModel requestModel) async {
+    var message='';
+    var url=Uri(
+      scheme: scheme,
+      host: host,
+      path: signUpPath,
+    );
+    try{
+      final response = await http.post(url, body: requestModel.toJson());
+
+      message=response.body;
+      if (response.statusCode==201){
+        return SignupResponseModel.fromJson(json.decode(response.body));
+      }
+      else{
+        throw Exception("Failed to load data ${response.statusCode}");
+      }
+    }
+    catch(e){
+      rethrow;
+    }
+
+  }
+}
