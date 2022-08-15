517a5bc8534c9ffbedb95f77c56341e208ff0cf7
Nana Kwasi <61452366+Z3ROD4RK@users.noreply.github.com>
2022-08-15 00:26:24 +0000

    Update event_page.dart
    Basic template for flyer holders

--- lib/home/event_page.dart
+++ lib/home/event_page.dart
@@ -5,8 +5,72 @@ class EventsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey,
+    return Scaffold(
+      body: Wrap(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              child: Text('Trending',
+                  style:
+                      TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+            ),
+          ),
+          Wrap(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(20.0),
+                width: 150.0,
+                height: 200.0,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                child: Text('Event', style: TextStyle(fontSize: 30.0)),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(20.0),
+                width: 150.0,
+                height: 200.0,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8)
+                ),
+                child: Text('Event', style: TextStyle(fontSize: 30.0)),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(20.0),
+                width: 150.0,
+                height: 200.0,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8)
+                ),
+                child: Text('Event', style: TextStyle(fontSize: 30.0)),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(20.0),
+                width: 150.0,
+                height: 200.0,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8)
+                ),
+                child: Text('Event', style: TextStyle(fontSize: 30.0)),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
