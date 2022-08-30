609192a9edb850c983b6c668cea7520dae4905a1
Max <maxotuteye12@gmail.com>
2022-08-30 22:24:27 +0000

    so many changes i dunno where to begin please call me

--- lib/home/event_page.dart
+++ lib/home/event_page.dart
@@ -2,37 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:matchmaking_demo/api/api_service_events.dart';
 import 'package:matchmaking_demo/components/events_details/events_details.dart';
 import 'package:matchmaking_demo/utils/variables.dart';
+import 'package:matchmaking_demo/models/events_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-
-class EventsPage extends StatelessWidget {
+class EventsPage extends StatefulWidget {
   const EventsPage({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => EventsPageState();
+}
+
+class EventsPageState extends State<EventsPage> {
+  final prefs = SharedPreferences.getInstance();
+  late Future<List<Event>> _futureEvents;
+  int eventCount = 4;
+
+  @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        itemCount: FlyerURLs.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, childAspectRatio: 150.0 / 200.0),
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => EventsDetails()));
-              },
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                margin: EdgeInsets.all(20.0),
-                width: 150.0,
-                height: 200.0,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(FlyerURLs[index].urls),
-                      fit: BoxFit.fill,
-                    ),
-                    color: Colors.grey,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(8)),
-              ));
-        });
+    _futureEvents = getEvents('');
+
+    return FutureBuilder<List<Event>>(
+      future: _futureEvents,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return GridView.builder(
+              itemCount: snapshot.data!.length - 1,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, childAspectRatio: 150.0 / 200.0),
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EventsDetails()));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      margin: EdgeInsets.all(20.0),
+                      width: 150.0,
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(FlyerURLs[index].urls),
+                            fit: BoxFit.fill,
+                          ),
+                          color: Colors.grey,
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8)),
+                    ));
+              });
+        }
+        return Text("Loading");
+      },
+    );
   }
 }
