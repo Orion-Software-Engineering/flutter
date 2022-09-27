5cca177feae5b34fc3eea81d3a26d2b526db4d55
kwasimbnyarko <51910665+kwasimbnyarko@users.noreply.github.com>
2022-09-27 05:38:39 +0000

    edit interest dialog size fix

--- lib/components/profile/interests/interest_field.dart
+++ lib/components/profile/interests/interest_field.dart
@@ -88,8 +88,8 @@ class _InterestFieldState extends State<InterestField> {
                             padding: const EdgeInsets.all(8.0),
                             child: Container(
                               color: Theme.of(context).primaryColor,
-                              height: MediaQuery.of(context).size.height * 0.38,
-                              width: MediaQuery.of(context).size.width * 0.8,
+                              height: 400,
+                              width: 400,
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
